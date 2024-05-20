8bfde87e56f512551ca5d28687cf89a61e44812b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-20 17:08:34 +0100

    Create StudyPage component

---
+++ src/components/studyPage/StudyPage.tsx
@@ -0,0 +1,15 @@
+// import test from "../../assets/82620866_p0_master1200.jpg";
+import { useRef } from "react";
+import test from "../../assets/82620866_p0_master1200.jpg";
+
+import styles from "./studyPage.module.css";
+// const canvasRef = useRef(null)
+export function StudyPage() {
+  return (
+    <>
+      <p>Studying</p>
+      <img className={styles.image} src={test} alt="" />
+      <canvas></canvas>
+    </>
+  );
+}
