c1e3aaf6e3797e1e504e089052271b0fe11e701b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-20 17:09:24 +0100

    Add ref to canvas

--- src/components/studyPage/StudyPage.tsx
+++ src/components/studyPage/StudyPage.tsx
@@ -3,13 +3,13 @@ import { useRef } from "react";
 import test from "../../assets/82620866_p0_master1200.jpg";
 
 import styles from "./studyPage.module.css";
-// const canvasRef = useRef(null)
+const canvasRef = useRef(null);
 export function StudyPage() {
   return (
     <>
       <p>Studying</p>
       <img className={styles.image} src={test} alt="" />
-      <canvas></canvas>
+      <canvas ref={canvasRef}></canvas>
     </>
   );
 }
