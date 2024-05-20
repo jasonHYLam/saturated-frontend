6d96b31601036a685a84733028153eedea1c89ac
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-20 17:36:41 +0100

    Create state for mouse position

--- src/components/studyPage/StudyPage.tsx
+++ src/components/studyPage/StudyPage.tsx
@@ -1,15 +1,25 @@
-// import test from "../../assets/82620866_p0_master1200.jpg";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import test from "../../assets/82620866_p0_master1200.jpg";
 
 import styles from "./studyPage.module.css";
-const canvasRef = useRef(null);
 export function StudyPage() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvasContext = canvasRef.current.getContext("2d");
+    }
+    setIsLoaded(true);
+  }, []);
   return (
     <>
       <p>Studying</p>
+
       <img className={styles.image} src={test} alt="" />
-      <canvas ref={canvasRef}></canvas>
+      {isLoaded ? <canvas ref={canvasRef}></canvas> : null}
     </>
   );
 }
