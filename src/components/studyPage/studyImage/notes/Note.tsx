9e64176258e22f9749a6dd56215a9b5bbcf97a4d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-29 17:40:14 +0100

    Create edit and delete buttons for notes

--- src/components/studyPage/studyImage/notes/Note.tsx
+++ src/components/studyPage/studyImage/notes/Note.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import styles from "./note.module.css";
 export function Note({ note }) {
+  const [isActive, setIsActive] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  function handleHover() {
+    setIsHovered(true);
+  }
+  function handleClick() {
+    setIsActive(true);
+  }
   return (
     <>
-      <article className={styles.note}>
+      <article
+        className={styles.note}
+        onMouseOver={handleHover}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {isHovered ? (
+          <div>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        ) : null}
         <div
           style={{ backgroundColor: note.color }}
           className={styles.colorReference}
