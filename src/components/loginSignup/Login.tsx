4c4eeab02a37b8853972b1fc75cf9b83e8b373a3
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-18 20:49:16 +0100

    Add fetch to login

--- src/components/loginSignup/Login.tsx
+++ src/components/loginSignup/Login.tsx
@@ -1,12 +1,35 @@
 import { Link } from "react-router-dom";
+import { fetchData } from "../../helpers/fetchData";
+import { useForm } from "react-hook-form";
 
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  async function submitLogin(data) {
+    const credentials = JSON.stringify(data);
+    await fetchData("login", "POST", credentials);
+  }
+
   return (
     <>
-      <form action="">
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
+      <form onSubmit={handleSubmit(submitLogin)}>
+        <input
+          type="text"
+          placeholder="Email"
+          {...register("email", { required: true })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("email", { required: true })}
+        />
         <input type="submit" value="Login" />
+        {errors.email && <span>Please provide email</span>}
+        {errors.password && <span>Please provide password</span>}
       </form>
       <Link to={"/signup"}>Don't have an account? Sign up</Link>
     </>
