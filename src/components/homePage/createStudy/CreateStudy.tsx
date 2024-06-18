fd8efc8abdf3542ab2012bc7646a91b3ab8b3ed9
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-18 20:33:57 +0100

    Display image to upload on upload

--- src/components/homePage/createStudy/CreateStudy.tsx
+++ src/components/homePage/createStudy/CreateStudy.tsx
@@ -2,12 +2,23 @@ import { useState } from "react";
 
 export function CreateStudy() {
   const [showCreateStudy, setShowCreateStudy] = useState(false);
+  const [uploadedImage, setUploadedImage] = useState(null);
 
   function toggleCreateStudy() {
     setShowCreateStudy(!showCreateStudy);
   }
+
+  function selectImageToUpload(e) {
+    setUploadedImage(e.target.files[0]);
+  }
+
   return (
     <>
+      {uploadedImage && (
+        <div>
+          <img src={URL.createObjectURL(uploadedImage)} alt="" />
+        </div>
+      )}
       <button onClick={toggleCreateStudy}>Create a study</button>
       {showCreateStudy && (
         <div>
@@ -15,7 +26,7 @@ export function CreateStudy() {
           <form>
             <input type="text" placeholder="Title" />
             <input type="text" placeholder="Original link" />
-            <input type="file" />
+            <input type="file" onChange={selectImageToUpload} />
             <input type="submit" value="Create study" />
           </form>
         </div>
