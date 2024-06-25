529e4c877cf98609786afddad555b295cfbb0e43
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-25 15:06:52 +0100

    Add path to StudyPreivew link

--- src/components/homePage/studyList/studyPreview/StudyPreview.tsx
+++ src/components/homePage/studyList/studyPreview/StudyPreview.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export function StudyPreview({ study }) {
   return (
     <>
-      <Link to={study.id}>
+      <Link to={`/study/${study.id}`}>
         <p>Study:</p>
         <img src={study.thumbnailLink} alt="" />
         <p>{study.title}</p>
