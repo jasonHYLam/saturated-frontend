538d84a093142fdb0287311f13b3843e84490d98
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-30 21:44:23 +0100

    Add submitLogout functionality to HomePage

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,10 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import { fetchWithoutQueryOrImage } from "../../helpers/fetchData";
 import { CreateStudy } from "./createStudy/CreateStudy";
 import { StudyList } from "./studyList/StudyList";
 
 export function HomePage() {
+  const navigate = useNavigate();
+  async function submitLogout() {
+    const response = await fetchWithoutQueryOrImage("logout", "POST");
+
+    if (response.ok || response instanceof Error) {
+      navigate("/error");
+    }
+
+    navigate("/login");
+  }
+
   return (
     <>
       <p>Home page</p>
+      <button onClick={submitLogout}>Logout</button>
       <CreateStudy />
       <StudyList />
     </>
