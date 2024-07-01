a759434946f8254e60db8c5eb6b25b5080a59ca5
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-01 20:39:00 +0100

    Add props to ToggleColorMode

--- src/components/studyPage/toggleColorMode/ToggleColorMode.tsx
+++ src/components/studyPage/toggleColorMode/ToggleColorMode.tsx
@@ -1,4 +1,14 @@
-export function ToggleColorMode({ setColorMode, colorMode }) {
+type ColorModeOptions = "grayscale" | "color";
+
+interface ToggleColorModeProps {
+  setColorMode: React.Dispatch<React.SetStateAction<string>>;
+  colorMode: ColorModeOptions;
+}
+
+export function ToggleColorMode({
+  setColorMode,
+  colorMode,
+}: ToggleColorModeProps) {
   return (
     <>
       {colorMode === "color" ? (
