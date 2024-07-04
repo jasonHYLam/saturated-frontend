330f138aa03aea472523a1529972d5581da794a4
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-04 15:25:53 +0100

    Update root cs

--- src/components/loginSignup/Login.tsx
+++ src/components/loginSignup/Login.tsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { fetchWithQuery } from "../../helpers/fetchData";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { GuestLogin } from "./GuestLogin";
+import styles from "./login.module.css";
 
 export function Login() {
   interface FormInput {
@@ -40,23 +41,25 @@ export function Login() {
 
   return (
     <>
-      <form onSubmit={handleSubmit(submitLogin)}>
-        <input
-          type="text"
-          placeholder="Email"
-          {...register("email", { required: true })}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          {...register("password", { required: true })}
-        />
-        <input type="submit" value="Login" />
-        {errors.email && <span>Please provide email</span>}
-        {errors.password && <span>Please provide password</span>}
-      </form>
-      <Link to={"/signup"}>Don't have an account? Sign up</Link>
-      <GuestLogin />
+      <section className={styles.container}>
+        <form onSubmit={handleSubmit(submitLogin)} className={styles.form}>
+          <input
+            type="text"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password", { required: true })}
+          />
+          <input type="submit" value="Login" />
+          {errors.email && <span>Please provide email</span>}
+          {errors.password && <span>Please provide password</span>}
+        </form>
+        <Link to={"/signup"}>Don't have an account? Sign up</Link>
+        <GuestLogin />
+      </section>
     </>
   );
 }
