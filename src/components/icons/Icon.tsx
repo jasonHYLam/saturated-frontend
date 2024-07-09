84927a86cb646058e03d663ff2b21f752d7a0ecf
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-09 12:50:19 +0100

    Add logo to homepage and login

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -2,8 +2,12 @@ import styles from "./icon.module.css";
 
 interface IconProps {
   iconPath: string;
+  size?: string;
 }
 
-export function Icon({ iconPath }: IconProps) {
-  return <img className={styles.icon} src={iconPath} alt="" />;
+export function Icon({ iconPath, size }: IconProps) {
+  let iconStyle = `${styles.small}`;
+  if (size === "large") iconStyle = `${styles.large}`;
+
+  return <img className={iconStyle} src={iconPath} alt="" />;
 }
--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -2,5 +2,5 @@ import { Icon } from "./Icon";
 import LogoPath from "../../assets/SaturatedLogo.png";
 
 export function Logo() {
-  return <Icon iconPath={LogoPath} />;
+  return <Icon iconPath={LogoPath} size="large" />;
 }
