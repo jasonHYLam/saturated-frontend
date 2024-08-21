f44370ce7efdc1f883424418e1aa121704416547
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-21 20:46:30 +0100

    Change imageFit state variable to imageFitMode

--- src/components/studyPage/StudyPage.tsx
+++ src/components/studyPage/StudyPage.tsx
@@ -40,7 +40,7 @@ export function StudyPage() {
     g: 0,
     b: 0,
   });
-  const [imageFit, setImageFit] = useState();
+  const [imageFitMode, setImageFitMode] = useState("fitWidth");
   const [colorMode, setColorMode] = useState<ColorModes>("color");
   const [hoveredMarkerAndNoteID, setHoveredMarkerAndNoteID] = useState<
     number | null
